880371332e695360630567de209b018f1519fb25
corenel <xxdsox@gmail.com>
2019-01-24 23:18:42 +0800

    Update build script

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build -t corenel/pytorch:cu90-pytorch1.0.0  -f Dockerfile .
-docker build -t corenel/pytorch:cu90-pytorch-nightly -f Dockerfile --build-arg BUILD_NIGHTLY=true .
+docker build -t corenel/pytorch:cu90-pytorch1.0.0-distributed  -f Dockerfile .
+docker build -t corenel/pytorch:cu90-pytorch-nightly-distributed -f Dockerfile --build-arg BUILD_NIGHTLY=true .
