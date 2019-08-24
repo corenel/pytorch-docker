267c02dd88c9b9a0c93c166f38d5adf764fd7892
corenel <xxdsox@gmail.com>
2019-08-24 18:33:45 +0800

    Update to PyTorch 1.2.0

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build -t corenel/pytorch:cu90-pytorch1.0.0-distributed  -f Dockerfile .
-docker build -t corenel/pytorch:cu90-pytorch-nightly-distributed -f Dockerfile --build-arg BUILD_NIGHTLY=true .
+docker build -t corenel/pytorch:cu100-pytorch1.2.0-distributed  -f Dockerfile.cu100.py36 .
+docker build -t corenel/pytorch:cu100-pytorch-nightly-distributed -f Dockerfile.cu100.py36 --build-arg BUILD_NIGHTLY=true .
